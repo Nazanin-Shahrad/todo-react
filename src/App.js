83b14eef55f52664f800aecf82bc8bf44a5b1043
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2022-10-15 17:47:25 -0400

    add header form todolist components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import React , {useState} from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [input , setInput] = useState('');
+  const [todos , setTodos] = useState([]);
+  const [editTodo , setEditTodo]=useState('');
+  console.log("app",todos)
   return (
-    <div className="App">
+    <div className="container">
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form  
+          input={input} 
+          setInput={setInput} 
+          todos={todos}
+          setTodos={setTodos} 
+          editTodo={editTodo} setEditTodo={setEditTodo}/>
+        </div>
+        <div>
+          <TodoList 
+          todos={todos} 
+          setTodos={setTodos}  
+          setEditTodo={setEditTodo}/>
+
+        </div>
+      </div>
      
     </div>
   );
